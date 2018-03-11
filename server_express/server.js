d4d3ea76a6198fabb0aa54db511cd52c07e5e128
Akkawat Tanan <kop_akkawat@hotmail.com>
2018-03-11 23:17:14 +0700

    create server_express

---
+++ server_express/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true })) 
+app.use(bodyParser.json({ type: '*/*' }));
+
+app.get('/', (req, res) => {
+    res.send('Root Page')
+})
+
+app.use((req, res, next) => {    
+    console.log('middle ware')    
+    next() 
+}) 
+
+app.get('/member', (req, res) => {
+    res.send('Member Page')
+})
+
+app.get('/order', (req, res) => {
+    res.send('Order Page')
+})
+
+app.get('/order/:orderId', (req, res) => {
+    res.send('Order Page id=' + req.params.orderId)
+})
+
+app.post('/order', (req, res) => {
+    console.log(req.body)
+    res.json({ status: 'ok' })
+})
+
+app.listen(8000, () => {
+    console.log('ready on http://localhost:8000')
+})
