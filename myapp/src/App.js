5339c9609300ee1cf992a861603d67cdc62899e8
Akkawat Tanan <kop_akkawat@hotmail.com>
2018-03-13 00:16:49 +0700

    implement set state.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 //import './App.css';
 import Header from './Header'
+import FbButton from './FbButton'
 
 class App extends Component {
+  state = { like: 0, dislike: 0 }
   render() {
     return (
       <div>
-        <Header title="React"/>
-        Hello React
+        <Header/>
+        <h4>Like : {this.state.like}</h4>        
+        <h4>DisLike: {this.state.dislike}</h4>        
+        <FbButton handleClick={this.onLike} caption="Like" />        
+        <FbButton handleClick={this.onDisLike} caption="DisLike"/> 
       </div>
     );
   }
+
+  onLike = () => {
+    this.setState({ like: this.state.like + 1 })
+  }
+  
+  onDisLike = () => {
+    this.setState({ dislike: this.state.dislike + 1 })
+  }
+  
 }
 
 export default App;
--- myapp/src/Header.js
+++ myapp/src/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return(
             <div>
-                <h1>This is Header { this.props.title }</h1>
+                <h1>This is Header</h1>
             </div>
         )
     }
