5579c93c09cc87d60ab726d1346fce0daa549216
Akkawat Tanan <kop_akkawat22@hotmail.com>
2018-04-10 20:10:04 +0700

    using redux and create store.

--- photoalbum/src/index.js
+++ photoalbum/src/index.js
@@ -5,12 +5,34 @@ import registerServiceWorker from './registerServiceWorker'
 
 import { Router, browserHistory } from 'react-router'
 import routes from './routes'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+function countAge(state = 0, action){
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(countAge);
+store.subscribe(() => { console.log('subscribe', store.getState()) });
+console.log('befor dispatch', store.getState());
+store.dispatch({
+    type: 'INCREMENT'
+})
 
 ReactDOM.render(
+    <Provider store={store}>
         <Router
             history={browserHistory}
             routes={routes}
-        />,
+        />
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
